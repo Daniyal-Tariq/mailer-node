f47dadf49f8c16103003908946a6c73a81bf0989
Ammar <maansaridot@gmail.com>
2020-05-06 05:29:47 +0500

    nodoemailer-mailgun-transport package with 2nd api

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+const mailGun = require("nodemailer-mailgun-transport");
+const cors = require("cors");
 
 const app = express();
 
@@ -13,64 +14,102 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.listen(port, () => {
-    console.log('We are live on port 4444');
+  console.log("We are live on port 4444");
 });
 
+app.get("/", (req, res) => {
+  res.send("Welcome to my api");
+});
 
+app.post("/api/v2", async (req, res) => {
+  console.log("/api/v2 begins");
+  const data = req.body;
 
+  const auth = {
+    auth: {
+      api_key: "",
+      domain: "",
+    },
+  };
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my api');
-})
+  const transporter = nodemailer.createTransport(mailGun(auth));
 
-app.post('/api/v1', (req, res) => {
-    var data = req.body;
-
-    // create reusable transporter object using the default SMTP transport
-    let smtpTransport = nodemailer.createTransport({
-        host: 'mail.bytescave.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: 'contact@bytescave.com', // generated ethereal user
-            pass: 'Fuck2916$'  // generated ethereal password
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
-
-    // var smtpTransport = nodemailer.createTransport({
-    //     host: 'mail.YOURDOMAIN.com',
-    //     service: 'privateemail',
-    //     port: 465,
-    //     auth: {
-    //         user: 'contact@bytescave.com',
-    //         pass: 'Fuck2916$'
-    //     }
-    // });
-
-    var mailOptions = {
-
-        from: data.email,
-        to: 'ENTER_YOUR_EMAIL',
-        subject: 'ENTER_YOUR_SUBJECT',
-        html: `<p>${data.firstName}</p>
+  const mailOptions = {
+    from: data.email,
+    to: "maansaridot@gmail.com",
+    subject: "Test email - Subject",
+    text: "I would like to get in touch with you!",
+    html: `
+        <h3>This is a test email</h3>
+        <p>${data.firstName}</p>
         <p>${data.lastName}</p>
         <p>${data.companyName}</p>
         <p>${data.email}</p>
-          <p>${data.phoneNumber}</p>
-          <p>${data.message}</p>`
-    };
+        <p>${data.phoneNumber}</p>
+        <p>${data.message}</p>
+        `,
+  };
 
-    smtpTransport.sendMail(mailOptions,
-        (error, response) => {
-            if (error) {
-                res.send(error)
-            } else {
-                res.send('Success')
-            }
-            smtpTransport.close();
-        });
+  transporter.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      console.log("Message did not send, %s", error);
+      res.send(error);
+    } else {
+      console.log("Message sent: %s", response);
+      res.send("Success");
+    }
+  });
+});
 
-})
+app.post("/api/v1", async (req, res) => {
+  var data = req.body;
+
+  //   create reusable transporter object using the default SMTP transport
+  let smtpTransport = nodemailer.createTransport({
+    name: "www.bytescave.com",
+    host: "mail.bytescave.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "contact@bytescave.com", // generated ethereal user
+      pass: "Fuck2916$", // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  // Verify connection configuration
+  await smtpTransport.verify(function (error, success) {
+    if (error) {
+      console.log("Server is NOT ready to take our messages");
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
+  var mailOptions = {
+    from: data.email,
+    to: "contact@bytescave.com",
+    subject: "Test Subject",
+    text: "Hello world?",
+    html: `<p>${data.firstName}</p>
+          <p>${data.lastName}</p>
+          <p>${data.companyName}</p>
+          <p>${data.email}</p>
+            <p>${data.phoneNumber}</p>
+            <p>${data.message}</p>`,
+  };
+
+  await smtpTransport.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      console.log("Message did not send, %s", error);
+      res.send(error);
+    } else {
+      console.log("Message sent: %s", response);
+      res.send("Success");
+    }
+    smtpTransport.close();
+  });
+});
